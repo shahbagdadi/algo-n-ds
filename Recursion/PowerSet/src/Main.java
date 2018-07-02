422d0cd610020502fd653a59e3ebb3363dfbaf4b
Shahnawaz Bagdadi <sbagdadi@netpace.com>
2018-07-02 13:56:39 -0700

    Recusion problems done in class on 07/01/2018

--- Recursion/PowerSet/src/Main.java
+++ Recursion/PowerSet/src/Main.java
@@ -1,26 +1,24 @@
-import java.util.List;
-
 public class Main {
 
 
     public void printAllSubsets(char[] a)
     {
         char[] output = new char[a.length];
-        PASHelper(a,0,output,0);
+        helper(a,output,0,0);
     }
 
-    public void PASHelper(char[] a , int r, char[] output, int w)
+    public void helper(char[] a , char[] o, int r, int w)
     {
         if (r == a.length) {
             System.out.print("{");
             for (int i = 0; i < w; i++)
-                System.out.print(output[i] + " ");
+                System.out.print(o[i] + " ");
             System.out.println("}");
             return;
         }
-        PASHelper(a,r+1,output,w);
-        output[w] = a[r];
-        PASHelper(a, r+1, output, w+1);
+        helper(a,o, r+1,w);
+        o[w] = a[r];
+        helper(a, o,r+1,  w+1);
     }
 
     public static void main(String[] args) {
