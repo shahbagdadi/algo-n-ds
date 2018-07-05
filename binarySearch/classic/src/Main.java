dce23a25dfde0439a90d8bc059863bc516a8b5c9
Shahnawaz Bagdadi <sbagdadi@gopro.com>
2018-07-05 11:49:21 -0700

    Added all 3 templates of binary search

--- binarySearch/classic/src/Main.java
+++ binarySearch/classic/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public int search(int[] nums, int target) {
+    public int searchRecursive(int[] nums, int target) {
         return helper(nums, 0, nums.length - 1, target);
     }
 
@@ -30,13 +30,45 @@ public class Main {
         return -1;
     }
 
+    int binarySearch2(int[] a, int t) {
+        if (a == null || a.length == 0)
+            return -1;
+
+        int l = 0, h = a.length;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (a[m] == t) return m;
+            else if (a[m] < t) l = m + 1;
+            else h = m;
+        }
+        if(l != a.length && a[l] == t) return l;
+        return -1;
+    }
+
+    int binarySearch3(int[] a, int t) {
+        if (a == null || a.length == 0)
+            return -1;
+
+        int l = 0, h = a.length -1;
+        while (l + 1 < h) {
+            int m = l + (h - l) / 2;
+            if (a[m] == t) return m;
+            else if (a[m] < t) l = m + 1;
+            else h = m;
+        }
+        if(a[l] == t) return l;
+        if(a[h] == t) return h;
+        return -1;
+    }
 
     public static void main(String[] args) {
 
         Main m = new Main();
         int[] a = new int[]{-4, 2, 6, 9, 10};
         int t = 6;
-        System.out.println(m.search(a, t));
+        System.out.println(m.searchRecursive(a, t));
         System.out.println(m.binarySearch1(a, t));
+        System.out.println(m.binarySearch2(a, t));
+        System.out.println(m.binarySearch3(a, t));
     }
 }
