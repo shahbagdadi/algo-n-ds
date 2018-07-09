04530145da54c027f9fed2af71488b91bdee15fd
Shahnawaz Bagdadi <sbagdadi@gopro.com>
2018-07-09 12:13:31 -0700

    Level order traversal

--- Trees/LevelOrderTraversal/src/Main.java
+++ Trees/LevelOrderTraversal/src/Main.java
@@ -1,21 +1,30 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
-
+    /**
+     *
+     * Time - O(n) - where n is number of nodes
+     * Space - O(n) - when n is max number of nodes on same level
+     */
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> lst = new ArrayList<>();
-        ArrayList<TreeNode> ndp = new ArrayList<>();
-        ArrayList<TreeNode> ndc = new ArrayList<>();
-        ndp.add(root);
-        while (!ndp.isEmpty())
-        {
-            TreeNode n = ndp.remove(0);
-            
-
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) return result;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i<levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            result.add(subList);
         }
-
+        return result;
     }
 
     public static void main(String[] args) {
@@ -24,10 +33,6 @@ public class Main {
         Main m = new Main();
         List<List<Integer>> l = m.levelOrder(n);
         for (List<Integer> lst : l)
-        {
-            for (Integer i : lst)
-                System.out.print(i);
-            System.out.println();
-        }
+            System.out.println(lst);
     }
 }
