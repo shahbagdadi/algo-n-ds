cec318a73e1f54153be77a40712dc95bf6dadfaf
Shahnawaz Bagdadi <sbagdadi@gopro.com>
2018-07-20 12:51:37 -0400

    Added code for testing

--- Trees/lowestCommonAncestor/src/Main.java
+++ Trees/lowestCommonAncestor/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root;
+        if (root == null || root.val == p.val || root.val == q.val) return root;
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
         return left == null ? right : right == null ? left : root;
@@ -9,6 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+    TreeNode n = Codec.deserialize("3 5 1 6 2 0 8 n n 7 4");
+    Main m = new Main();
+    System.out.println(m.lowestCommonAncestor(n,new TreeNode(5), new TreeNode(1)).val);
+    System.out.println(m.lowestCommonAncestor(n,new TreeNode(5), new TreeNode(4)).val);
     }
 }
