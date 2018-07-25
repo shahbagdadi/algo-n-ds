859bb7401cc3d97babd53dfb4659d40d55c20301
Shahnawaz Bagdadi <sbagdadi@gopro.com>
2018-07-24 21:13:16 -0400

    Updated Island count

--- IslandCount/src/Island.java
+++ IslandCount/src/Island.java
@@ -1,64 +1,58 @@
 /**
- * Created by sbagdadi on 1/13/18.
+ * Time - O(m*n)
+ * Space - O(n) - the size of longest island
  */
 public class Island {
 
+    private static final int[][] steps = new int[][]{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
-    private static final int[] row = {1, 1, 1, 0, -1, -1, -1, 0};
-    private static final int[] col = {-1, 0, 1, 1, 1, 0, -1, -1};
-    int [][] adj;
-
-    public Island(int[][] adj){
-        this.adj = adj;
-    }
-
-    private boolean isValidNode(int r, int c) {
-        if (r < 0 || c < 0 || r > adj.length - 1 || c > adj[0].length - 1)
+    private boolean isValidNode(char[][] grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length)
             return false;
-        else
-            return true;
+        return true;
     }
 
-    public int getIslandCount(){
+    public int numIslands(char[][] grid) {
         int count = 0;
-        for (int i = 0; i < adj.length; i++) {
-            for (int j = 0; j < adj[0].length; j++) {
-                if (adj[i][j] == 1) {
-                    DFS(i,j);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    //System.out.println("root - " + i + "," + j);
+                    DFS(grid, i, j);
                     count++;
                 }
             }
         }
         return count;
     }
-    private void DFS(int r, int c) {
-        if (isValidNode(r, c) == false ) return;
-        int root = adj[r][c];
-        System.out.print("[" + r + "," + c + "] ");
-        adj[r][c] = 0;
-        for (int rb : row) {
-            for (int cb : col) {
-                int rnum = r + rb;
-                int cnum = c + cb;
-                if (isValidNode(rnum, cnum) && adj[rnum][cnum] == 1) {
-                    System.out.print("[" + rnum + "," + cnum + "] ");
-                    adj[r][c] = 0;
-                    DFS(rnum, cnum);
-                }
+
+
+    private void DFS(char[][] grid, int r, int c) {
+        if (!isValidNode(grid, r, c)) return;
+        grid[r][c] = 0;
+        for (int[] step : steps) {
+            int row = r + step[0];
+            int col = c + step[1];
+            if (isValidNode(grid, row, col) && grid[row][col] == '1') {
+                //System.out.print("[" + row + "," + col + "] ");
+                grid[r][c] = 0;
+                DFS(grid, row, col);
             }
         }
     }
 
-
     public static void main(String[] args) {
-        int[][] adj = {
-                {1, 1, 0, 0, 0},
-                {0, 1, 0, 0, 1},
-                {1, 0, 0, 1, 1},
-                {0, 0, 0, 0, 0},
-                {1, 0, 1, 0, 1}
+        char l = '1';
+        char w = '0';
+        char[][] adj = {
+                {l, l, w, w, w},
+                {w, l, w, w, l},
+                {l, w, w, l, l},
+                {w, w, w, w, w},
+                {l, w, l, w, l}
         };
-        Island island = new Island(adj);
-        System.out.println("Island count " + island.getIslandCount());
+        Island island = new Island();
+        int cnt = island.numIslands(adj);
+        System.out.println("Island count " + cnt);
     }
 }
