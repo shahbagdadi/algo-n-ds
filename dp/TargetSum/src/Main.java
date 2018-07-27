64288e936ad6a2fb288792afa4bff624368b717e
Shahnawaz Bagdadi <sbagdadi@gopro.com>
2018-07-27 11:47:17 -0400

    TargetSum

---
+++ dp/TargetSum/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1);
+    }
+
+    public int subsetSum(int[] nums, int s) {
+        int[] dp = new int[s + 1];
+        dp[0] = 1;
+        for (int n : nums)
+            for (int i = s; i >= n; i--)
+                dp[i] += dp[i - n];
+        return dp[s];
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        int[] a = new int[]{1, 2, 3, 4, 5};
+        int ans = m.findTargetSumWays(a, 3);
+        System.out.println(ans);
+    }
+}
