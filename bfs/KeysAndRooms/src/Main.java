58106d84e16827de6301d7e0fc3558ccf24c51e7
Shahnawaz Bagdadi <sbagdadi@gopro.com>
2018-07-27 18:30:23 -0400

    Keys and Rooms

---
+++ bfs/KeysAndRooms/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        while (!q.isEmpty()) {
+            int room = q.poll();
+            visited[room] = true;
+            for (int i :rooms.get(room)) {
+                if (!visited[i])
+                    q.offer(i);
+            }
+        }
+        for (boolean flag : visited)
+            if (!flag) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> r0 = new ArrayList<>();
+        r0.add(1);
+        r0.add(3);
+        List<Integer> r1 = new ArrayList<>();
+        r1.add(3);
+        r1.add(0);
+        r1.add(1);
+        List<Integer> r2 = new ArrayList<>();
+        r2.add(2);
+        List<Integer> r3 = new ArrayList<>();
+        r3.add(0);
+        List<List<Integer>> rooms = new ArrayList<>();
+        rooms.add(r0);
+        rooms.add(r1);
+        rooms.add(r2);
+        rooms.add(r3);
+        Main m = new Main();
+        System.out.println(m.canVisitAllRooms(rooms));
+
+    }
+}
