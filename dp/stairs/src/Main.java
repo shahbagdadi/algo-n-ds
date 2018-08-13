d8eb7b8bcaba2f7d4a04efba5746f44aa24c5615
Shahnawaz Bagdadi <sbagdadi@gopro.com>
2018-08-13 10:45:42 -0700

    stairs solution using dp template

--- dp/stairs/src/Main.java
+++ dp/stairs/src/Main.java
@@ -1,29 +1,36 @@
 public class Main {
 
     public int climbStairs(int n) {
-        if (n ==1) return 1;
-        if (n ==2) return 2;
+        if (n <= 2) return n;
         return climbStairs(n-1) + climbStairs(n-2);
     }
 
     public int climbStairsDP(int n) {
-        if (n ==1) return 1;
-        if (n ==2) return 2;
-        int dp1 = 1;
-        int dp2 = 2;
-        for (int i=3;i<n+1;i++)
+
+        if (n <= 2) return n;
+
+        // int[] dp = new int[n+1]
+        // dp[1] = 1;
+        // dp[2] = 2;
+        int step1 = 1;
+        int step2 = 2;
+        int ans = 0;
+        for (int i=3;i <= n;i++) // or (< dp.length)
         {
-            int dp3 = dp2 + dp1;
-            dp1 = dp2;
-            dp2 = dp3;
+            ans = step1 + step2; // dp[i] = dp[i-2] + dp[i-1]
+            step1 = step2;
+            step2 = ans;
         }
-        return dp2;
+        return ans;  // dp[i];
     }
 
     public static void main(String[] args) {
         Main m = new Main();
-//        int n = m.climbStairs(4);
-        int n = m.climbStairsDP(4);
-        System.out.println(n);
+        int n = 4;
+        int a1 = m.climbStairs(n);
+        System.out.println(a1);
+        int a2 = m.climbStairsDP(n);
+        System.out.println(a2);
+
     }
 }
