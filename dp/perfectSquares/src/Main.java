b06a801b327fda8807d5f5d9911a28d3ad02fe32
Shahnawaz Bagdadi <sbagdadi@netpace.com>
2018-08-18 18:08:06 -0700

    perfect square  dp

--- dp/perfectSquares/src/Main.java
+++ dp/perfectSquares/src/Main.java
@@ -1,26 +1,29 @@
+import java.util.Arrays;
+
 /**
  * Time - O(n*n^1/2) = O(n^3/2)
  * SPace - O(n)
  */
 public class Main {
 
-    public int numSquares(int n) {
-        int[] dp = new int[n + 1];
+    public int numSquares(int num) {
+        int[] dp = new int[num + 1];
         dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
+        for (int n = 1; n < dp.length; n++) {
             int min = Integer.MAX_VALUE;
-            for (int j = 1; i - j * j >= 0; j++){
-                min = Math.min(min, dp[i - j * j] + 1);
-                dp[i] = min;
-                System.out.println("dp[" + i + "] = " + min);
+            for (int i = 1; i * i <= n; i++){
+                min = Math.min(min, dp[n - i * i] + 1);
+                dp[n] = min;
+                //System.out.println("dp[" + n + "] = " + min);
             }
         }
-        return dp[n];
+        System.out.println(Arrays.toString(dp));
+        return dp[num];
     }
 
 
     public static void main(String[] args) {
         Main m = new Main();
-        System.out.println(m.numSquares(12));
+        System.out.println(m.numSquares(13));
     }
 }
