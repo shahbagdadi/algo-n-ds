b2c12cc66b289ac2b7ddeafa93662a33b119096e
Shahnawaz Bagdadi <sbagdadi@gopro.com>
2018-09-26 12:02:53 -0700

    minor modifications

--- Array/3Sum/src/Main.java
+++ Array/3Sum/src/Main.java
@@ -4,21 +4,26 @@ import java.util.List;
 
 public class Main {
 
+    // T - O(n^2) , S - O(1)
     public List<List<Integer>> threeSum(int[] a) {
+        // O(n logn)
         Arrays.sort(a);
+        System.out.println("Sorted : " + Arrays.toString(a));
         List<List<Integer>> lst = new ArrayList<>();
         int N = a.length;
+        //O(n^2)
         for (int i = 0; i + 2 < N; i++) {
             if (i > 0 && a[i] == a[i - 1]) continue;
             int sum = a[i];
             int p = i + 1, q = N - 1;
             while (p < q) {
-                System.out.println(sum + " " + a[p] + " " + a[q]);
                 int total = a[p] + a[q] + sum;
+                System.out.println(sum + " + " + a[p] + " + " + a[q] + " = " + total);
                 if (total == 0) {
                     lst.add(Arrays.asList(sum,a[p],a[q]));
                     p++;
                     q--;
+                    // skip to avoid getting same result for duplicates
                     while (p < N && a[p] == a[p - 1]) p++;
                     while (q > i && a[q] == a[q + 1]) q--;
                 }
@@ -31,20 +36,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] s = {-1, 0, 1, 2, -1, -4};
+          int[] s = {-1, 0, 1, 2, -1, -4};
 //        int [] s = {0,0,0,0};
 //        int [] s = {1,-1,-1,0};
 //        int [] s = {-2,0,1,1,2};
 //        int[] s = {-2, 0, 0, 2, 2};
         Main m = new Main();
         List<List<Integer>> lst = m.threeSum(s);
-        for (int i = 0; i < lst.size(); i++) {
-            List<Integer> integers = lst.get(i);
-            for (int j = 0; j < integers.size(); j++) {
-                Integer integer = integers.get(j);
-                System.out.print(integer + " , ");
-            }
-            System.out.println();
-        }
+        System.out.println(Arrays.toString(lst.toArray()));
     }
 }
